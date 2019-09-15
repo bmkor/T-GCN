aa7f64b2b3467e81f826f4bc1cb0c8706864da63
Benjamin <orbenappdev@gmail.com>
2019-09-15 10:02:38 +0800

    commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 import numpy.linalg as la
 from input_data import preprocess_data,load_sz_data,load_los_data
 from tgcn import tgcnCell
-#from gru import GRUCell 
+#from gru import GRUCell
 
 from visualization import plot_result,plot_error
 from sklearn.metrics import mean_squared_error,mean_absolute_error
@@ -18,7 +18,6 @@ import time
 time_start = time.time()
 ###### Settings ######
 flags = tf.app.flags
-FLAGS = flags.FLAGS
 flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
 flags.DEFINE_integer('training_epoch', 1, 'Number of epochs to train.')
 flags.DEFINE_integer('gru_units', 64, 'hidden units of gru.')
@@ -28,6 +27,8 @@ flags.DEFINE_float('train_rate', 0.8, 'rate of training set.')
 flags.DEFINE_integer('batch_size', 32, 'batch size.')
 flags.DEFINE_string('dataset', 'los', 'sz or los.')
 flags.DEFINE_string('model_name', 'tgcn', 'tgcn')
+flags.DEFINE_string('f','','kernel')
+FLAGS = flags.FLAGS
 model_name = FLAGS.model_name
 data_name = FLAGS.dataset
 train_rate =  FLAGS.train_rate
@@ -73,7 +74,7 @@ def TGCN(_X, _weights, _biases):
     output = tf.transpose(output, perm=[0,2,1])
     output = tf.reshape(output, shape=[-1,num_nodes])
     return output, m, states
-        
+
 ###### placeholders ######
 inputs = tf.placeholder(tf.float32, shape=[None, seq_len, num_nodes])
 labels = tf.placeholder(tf.float32, shape=[None, pre_len, num_nodes])
@@ -88,7 +89,7 @@ if model_name == 'tgcn':
     pred,ttts,ttto = TGCN(inputs, weights, biases)
 
 y_pred = pred
-      
+
 
 ###### optimizer ######
 lambda_loss = 0.0015
@@ -102,7 +103,7 @@ optimizer = tf.train.AdamOptimizer(lr).minimize(loss)
 
 ###### Initialize session ######
 variables = tf.global_variables()
-saver = tf.train.Saver(tf.global_variables())  
+saver = tf.train.Saver(tf.global_variables())
 #sess = tf.Session()
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
 sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
@@ -114,7 +115,7 @@ path1 = '%s_%s_lr%r_batch%r_unit%r_seq%r_pre%r_epoch%r'%(model_name,data_name,lr
 path = os.path.join(out,path1)
 if not os.path.exists(path):
     os.makedirs(path)
-    
+
 ###### evaluation ######
 def evaluation(a,b):
     rmse = math.sqrt(mean_squared_error(a,b))
@@ -123,11 +124,11 @@ def evaluation(a,b):
     r2 = 1-((a-b)**2).sum()/((a-a.mean())**2).sum()
     var = 1-(np.var(a-b))/np.var(a)
     return rmse, mae, 1-F_norm, r2, var
- 
-   
+
+
 x_axe,batch_loss,batch_rmse,batch_pred = [], [], [], []
 test_loss,test_rmse,test_mae,test_acc,test_r2,test_var,test_pred = [],[],[],[],[],[],[]
-  
+
 for epoch in range(training_epoch):
     for m in range(totalbatch):
         mini_batch = trainX[m * batch_size : (m+1) * batch_size]
@@ -151,16 +152,16 @@ for epoch in range(training_epoch):
     test_r2.append(r2_score)
     test_var.append(var_score)
     test_pred.append(test_output1)
-    
+
     print('Iter:{}'.format(epoch),
           'train_rmse:{:.4}'.format(batch_rmse[-1]),
           'test_loss:{:.4}'.format(loss2),
           'test_rmse:{:.4}'.format(rmse),
           'test_acc:{:.4}'.format(acc))
-    
-    if (epoch % 500 == 0):        
+
+    if (epoch % 500 == 0):
         saver.save(sess, path+'/model_100/TGCN_pre_%r'%epoch, global_step = epoch)
-        
+
 time_end = time.time()
 print(time_end-time_start,'s')
 
--- tgcn.py
+++ tgcn.py
@@ -33,7 +33,7 @@ class tgcnCell(RNNCell):
     def __call__(self, inputs, state, scope=None):
 
         with tf.variable_scope(scope or "tgcn"):
-            with tf.variable_scope("gates"):  
+            with tf.variable_scope("gates"):
                 value = tf.nn.sigmoid(
                     self._gc(inputs, state, 2 * self._units, bias=1.0, scope=scope))
                 r, u = tf.split(value=value, num_or_size_splits=2, axis=1)
@@ -47,13 +47,15 @@ class tgcnCell(RNNCell):
     def _gc(self, inputs, state, output_size, bias=0.0, scope=None):
         ## inputs:(-1,num_nodes)
         inputs = tf.expand_dims(inputs, 2)
+        print(inputs.shape)
         ## state:(batch,num_node,gru_units)
         state = tf.reshape(state, (-1, self._nodes, self._units))
         ## concat
         x_s = tf.concat([inputs, state], axis=2)
         input_size = x_s.get_shape()[2].value
+
         ## (num_node,input_size,-1)
-        x0 = tf.transpose(x_s, perm=[1, 2, 0])  
+        x0 = tf.transpose(x_s, perm=[1, 2, 0])
         x0 = tf.reshape(x0, shape=[self._nodes, -1])
         
         scope = tf.get_variable_scope()
